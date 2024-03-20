03fac7d288930241935cafb015c4b0da51f1f7ee
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-03-20 14:50:07 -0300

    installation bootstrap

--- app_produtos/models.py
+++ app_produtos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+EMBALAGEM_DE_VENDA = [
+    ('caixa', 'Caixa'),
+    ('duzia', 'Duzia'),
+    ('unidade', 'Unidade'),
+    ('kilo', 'Kilo'),
+    ('peca', 'Peça'),
+    ('embalagem', 'Embalagem'),
+    ('metro', 'Metro'),
+    ('outros', 'Outros'),
+]
+
+class Produtos(models.Model):
+    codigo = models.CharField(max_length=5, verbose_name='Código')
+    nome = models.CharField(max_length=150, verbose_name='Nome')
+    estoque = models.IntegerField(verbose_name='Estoque')
+    preco_custo = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Preço de custo')
+    preco_venda = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Preço de venda')
+    embalagem = models.CharField(max_length=35, choices=EMBALAGEM_DE_VENDA, verbose_name='Embalagem')
--- base_static/global/js/main.js
+++ base_static/global/js/main.js
@@ -1,3 +1,13 @@
-document.getElementById('open_btn').addEventListener('click', function(){
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
+function abrir_sidebar(){
+    document.getElementById('open_btn').addEventListener('click', function(){
+        document.getElementById('sidebar').classList.toggle('open-sidebar');
+    });
+}
+abrir_sidebar()
+
+function adiciona_cor() {
+   document.getElementById('side_items').addEventListener('click', function(){
+    document.getElementsByClassName('side_item').classList.add('active')
+   })
+}
+adiciona_cor()

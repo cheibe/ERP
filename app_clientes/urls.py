520e18bd6adc23834b5b4c7e19e3311b8f002d41
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-05 14:28:45 -0300

    urls clientes

--- app_clientes/urls.py
+++ app_clientes/urls.py
@@ -5,4 +5,7 @@ app_name = 'clientes'
 
 urlpatterns = [
     path('', views.clientes_views, name='clientes'),
+    path('adicionar/', views.adicionar_cliente, name='adicionar_cliente'),
+    path('editar/<cliente_id>', views.editar_cliente, name='editar_cliente'),
+    path('deletar/<cliente_id>', views.deletar_cliente, name='deletar_cliente')
 ]

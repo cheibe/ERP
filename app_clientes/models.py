2ebef38f97da81da96be856c2771b84947ed2780
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-11 09:31:35 -0300

    models fornecedores and fix models clientes

--- app_clientes/models.py
+++ app_clientes/models.py
@@ -3,14 +3,17 @@ from django.db import models
 class Cliente(models.Model):
     razao_social = models.CharField(max_length=150, verbose_name='Razão social')
     nome_fantasia = models.CharField(max_length=150, null=True,verbose_name='Nome fantasia')
-    rg = models.CharField(max_length=9, null=True, verbose_name='RG')
-    cnpj = models.CharField(max_length=14, null=True, verbose_name='CNPJ')
+    rg = models.CharField(max_length=9, null=True, blank=True, verbose_name='RG')
+    cnpj = models.CharField(max_length=14, null=True, blank=True, verbose_name='CNPJ')
     rua = models.CharField(max_length=50, verbose_name='Rua')
     numero = models.CharField(max_length=5, verbose_name='Numero')
     bairro = models.CharField(max_length=50, verbose_name='Bairro')
     cidade = models.CharField(max_length=50, verbose_name='Cidade')
     estado = models.CharField(max_length=2, verbose_name='Estado')
     cep = models.CharField(max_length=8, verbose_name='CEP')
-    complemento = models.CharField(max_length=100, null=True, verbose_name='Complemento')
+    complemento = models.CharField(max_length=100, null=True, blank=True, verbose_name='Complemento')
     telefone = models.CharField(max_length=11, verbose_name='Telefone')
-    telefone_fixo = models.CharField(max_length=11, null=True, verbose_name='Telefone fixo')
+    telefone_fixo = models.CharField(max_length=11, null=True, blank=True, verbose_name='Telefone fixo')
+
+    def __str__(self):
+        return self.nome_fantasia
--- app_fornecedores/models.py
+++ app_fornecedores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=120, verbose_name='Nome')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
+    documento = models.CharField(max_length=20, verbose_name='CPF/CNPJ')
+    rua = models.CharField(max_length=250, verbose_name='Logradouro')
+    numero = models.CharField(max_length=10, verbose_name='Número')
+    bairro = models.CharField(max_length=100, verbose_name='Bairro')
+    cidade = models.CharField(max_length=100, verbose_name='Cidade')
+    estado = models.CharField(max_length=2, verbose_name='Estado')
+
+    def __str__(self):
+        return self.nome

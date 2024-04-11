acab1c93c6427ac9f410521e69b2a6df8d778de2
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-11 15:17:13 -0300

    create view fornecedores all

--- app_fornecedores/views.py
+++ app_fornecedores/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from app_fornecedores.models import Fornecedor
+from app_fornecedores.forms import FornecedorForm, EditFornecedorForm
 
 def fornecedores_view(request):
+    fornecedores = Fornecedor.objects.all()
     return render(request, 'pages/fornecedores.html', context={
-        'title': 'Fornecedores'
+        'title': 'Fornecedores',
+        'fornecedores': fornecedores
     })

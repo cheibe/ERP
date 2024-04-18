58d36daaeeb1d6f38c76b5ef64d578681e49c21c
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-18 14:45:57 -0300

    create model

--- app_pagamentos/models.py
+++ app_pagamentos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from app_fornecedores.models import Fornecedor
 
-# Create your models here.
+PAGAMENTO_STATUS = [
+    ('pendente', 'Pendente'),
+    ('pago', 'Pago'),
+    ('cancelado', 'Cancelado'),
+]
+
+class Pagamento(models.Model):
+    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=255, verbose_name='Descrção')
+    valor = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor')
+    data_emissao = models.DateField(auto_now_add=True, verbose_name='Data de Emissão')
+    data_vencimento = models.DateField(verbose_name='Data de Vencimento')
+    status = models.CharField(max_length=30, choices=PAGAMENTO_STATUS, default='pendente')
+
+    def __str__(self):
+        return self.fornecedor

6dbb8bba6d679de8c45476c93a2b29e57a4210f8
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-18 09:46:21 -0300

    add urls and add html adicionar_fornecedor

--- app_fornecedores/urls.py
+++ app_fornecedores/urls.py
@@ -5,4 +5,7 @@ app_name = 'fornecedores'
 
 urlpatterns = [
     path('', views.fornecedores_view, name='fornecedores'),
+    path('adicionar', views.adicionar_fornecedor, name='adicionar_fornecedor'),
+    path('editar/<fornecedor_id>', views.editar_fornecedor, name='editar_fornecedor'),
+    path('deletar/<fornecedor_id>', views.delete_fornecedor, name='deletar_fornecedor'),
 ]

0fb025a89505feee87db52d671b8400a4fc0da69
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-18 09:38:32 -0300

    add views fornecedor

--- app_fornecedores/admin.py
+++ app_fornecedores/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from app_fornecedores.models import Fornecedor
 
-# Register your models here.
+
+admin.site.register(Fornecedor)
--- app_fornecedores/views.py
+++ app_fornecedores/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from app_fornecedores.models import Fornecedor
 from app_fornecedores.forms import FornecedorForm, EditFornecedorForm
+from django.contrib import messages
 
 def fornecedores_view(request):
     fornecedores = Fornecedor.objects.all()
@@ -8,3 +9,40 @@ def fornecedores_view(request):
         'title': 'Fornecedores',
         'fornecedores': fornecedores
     })
+
+def adicionar_fornecedor(request):
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST)
+        if form.is_valid():
+            novo_fornecedor = form.save()
+            messages.success(request, f'O fornecedor {novo_fornecedor.nome} foi cadastrado com sucesso!')
+            return redirect('fornecedores:fornecedores')
+        else:
+            form = FornecedorForm()
+
+    return render(request, 'pages/adicionar_fornecedor.html', context= {
+        'title': 'Adicionar fornecedor',
+        'form': form
+    })
+
+def editar_fornecedor(request, fornecedor_id):
+    fornecedor = get_object_or_404(Fornecedor, pk=fornecedor_id)
+    if request.method == 'POST':
+        form = EditFornecedorForm(request.POST, instance=fornecedor)
+        if form.is_valid():
+            fornecedor_edit = form.save()
+            messages.success(request, f'O fornecedor {fornecedor_edit.nome} foi editado com sucesso!')
+            return redirect('fornecedores:fornecedores')
+        else:
+            form = EditFornecedorForm(instance=fornecedor)
+    return render(request, 'pages/adicionar_fornecedor.html', context={
+        'title': 'Editar fornecedor',
+        'form': form
+    })
+
+def delete_fornecedor(request, fornecedor_id):
+    fornecedor = get_object_or_404(Fornecedor, pk=fornecedor_id)
+    
+    fornecedor.delete()
+    messages.success(request, f'O fornecedor foi deletado com sucesso!')
+    return redirect('fornecedores:fornecedores')

948debc43756c17536bed85b7f993b004264b5b6
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-24 15:58:04 -0300

    create model empresa

--- app_empresas/urls.py
+++ app_empresas/urls.py
@@ -4,5 +4,5 @@ from app_empresas import views
 app_name = 'empresas'
 
 urlpatterns = [
-    ...
+    path('', views.empresas_view, name='empresas')
 ]
--- app_empresas/views.py
+++ app_empresas/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render
+
+def empresas_view(request):
+    return render(request, 'pages/empresas.html', context={
+        'title': 'Empresas'
+    })

2dc274d895466afb187992c52ca7fc2a102e1e9c
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-24 16:55:24 -0300

    add null in field telefone_fixo and add name in adm

--- app_empresas/models.py
+++ app_empresas/models.py
@@ -5,6 +5,9 @@ class Empresa(models.Model):
     nome_fantasia = models.CharField(max_length=170, verbose_name='Nome fantasia')
     cnpj = models.CharField(max_length=14, verbose_name='CNPJ')
     ie = models.CharField(max_length=9, verbose_name='Inscrição estadual')
-    telefone_fixo = models.CharField(max_length=10, verbose_name='Telefone fixo')
+    telefone_fixo = models.CharField(max_length=10, verbose_name='Telefone fixo', null=True)
     telefone_movel = models.CharField(max_length=12, verbose_name='Telefone movél')
     observacao = models.CharField(max_length=255, verbose_name='Observação')
+
+    def __str__(self):
+        return self.nome_fantasia

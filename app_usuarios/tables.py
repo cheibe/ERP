f7f02396d43bc6f5a0d4d4e0da9b5f465cdb7158
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-04-26 09:19:06 -0300

    fix tables users

--- app_usuarios/tables.py
+++ app_usuarios/tables.py
@@ -2,12 +2,13 @@ import django_tables2 as tables
 from django.contrib.auth.models import User
 
 class UsuarioTable(tables.Table):
-    first_name = tables.Column(verbose_name='Primeiro nome', orderable=False)
-    username = tables.Column(verbose_name='Usuário', orderable=False)
+    nome = tables.Column(verbose_name='Nome', orderable=False)
     email = tables.Column(verbose_name='E-mail', orderable=False)
+    is_admin = tables.Column(verbose_name='Admin', orderable=False)
+    is_comum = tables.Column(verbose_name='Comum', orderable=False)
     opcao = tables.TemplateColumn(template_name='pages/botao_acoes_usuarios.html', verbose_name='Opções', orderable=False)
 
     class Meta:
         model = User
         attrs = {'class': 'table table-bordered table-hover'}
-        fields = ('first_name', 'username', 'email')
+        fields = ('nome', 'email', 'is_admin', 'is_comum')

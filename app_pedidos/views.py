318a034f853fc581cbb9ea116d4f593e9c2dd608
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-05-20 13:53:26 -0300

    half pedidos

--- app_pedidos/views.py
+++ app_pedidos/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from app_produtos.models import Produtos
 
 def pedidos_view(request):
     return render(request, 'pages/pedidos.html', context={
         'title': 'Pedidos'
+    })
+
+def adicionar_pedido(request):
+    produtos = Produtos.objects.all()
+    return render(request, 'pages/adicionar_pedido.html', context={
+        'title': 'Adicionar pedido',
+        'produtos': produtos
     })
